07db505ed5836518b9940bf00fce2137c59b68df
hasan <hasanocal7@gmail.com>
2023-12-19 01:17:15 +0300

    Frontend | Made Axios Connection for Register

--- client/src/components/SignUp.vue
+++ client/src/components/SignUp.vue
@@ -2,12 +2,17 @@
   <div class="d-flex align-center justify-center" style="height: 100vh">
     <v-sheet width="400" class="mx-auto">
       <v-form fast-fail @submit.prevent="register">
-        <v-text-field v-model="name" label="Name"></v-text-field>
-        <v-text-field v-model="email" label="Email"></v-text-field>
-        <v-text-field v-model="password" label="Password"></v-text-field>
+        <v-text-field v-model="name" label="Name" name="name"></v-text-field>
+        <v-text-field v-model="email" label="Email" name="email"></v-text-field>
+        <v-text-field
+          v-model="password"
+          label="Password"
+          name="password"
+        ></v-text-field>
         <v-select
           v-model="authority"
           label="Authority"
+          name="authority"
           :items="['admin', 'editor']"
         ></v-select>
         <v-btn type="submit" color="primary" block class="mt-2">Sign Up</v-btn>
@@ -28,7 +33,34 @@ export default {
     };
   },
   methods: {
-    register() {},
+    async register() {
+      const data = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        authority: this.authority,
+      };
+
+      try {
+        const response = await axios.post(
+          "http://localhost:3000/auth/register",
+          data
+        );
+        console.log(response);
+        this.$router.push("/login");
+      } catch (error) {
+        console.error(error);
+        if (
+          error.response &&
+          error.response.data &&
+          error.response.data.response
+        ) {
+          console.error("Server Response:", error.response.data.response);
+        } else {
+          console.error("Unexpected Error:", error.message);
+        }
+      }
+    },
   },
 };
 </script>
